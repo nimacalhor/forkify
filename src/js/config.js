48a2666b28ac96d9f1efea1c6850125028c28be6
nimacalhor <nimakalhor25@gmail.com>
2022-07-28 12:23:35 +0430

    #

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'f7005994-4290-46b3-9069-6f6751d8b53f';
+export const MODAL_CLOSE_SEC = 2.5;
